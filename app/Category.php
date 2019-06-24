002c6b7dca38528a6750b3e950226f23b5319246
moman13 <35956421+moman13@users.noreply.github.com>
2019-06-24 21:51:24 +0300

    Update Category.php

--- app/Category.php
+++ app/Category.php
@@ -8,18 +8,18 @@ class Category extends Model
 {
     protected $fillable = [];
 
-  //   public function parent(){
-		// 	return $this->hasMany('App\categories','parent_id');
-		// }
+    public function parent(){
+		 	return $this->hasMany('App\Category','parent_id');
+		 }
 
-		// public function children(){
-		// 	return $this->belongsTo('App\categories','parent_id');
-		// }
+		 public function children(){
+		 	return $this->belongsTo('App\Category','parent_id');
+		 }
 
 		public function products() {return $this->hasMany('App\Product', 'subcategories_id');}
 
 
-		public function subcategories(){ return $this->hasMany('App\Subcategory', 'categories_id'); }    
+		//public function subcategories(){ return $this->hasMany('App\Subcategory', 'categories_id'); }    
 
 	
 
